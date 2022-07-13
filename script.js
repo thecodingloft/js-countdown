2f53d04e50bf9a364b321c55b9f9b0915ed414b3
Samuel <samuelschanderl@Samuels-MacBook-Pro.local>
2022-07-13 23:08:39 +0200

    almost finished js - need to stop input after string has 4 values

--- script.js
+++ script.js
@@ -4,35 +4,68 @@ let seconds = document.getElementById('seconds-count')
 
 const getTime = () =>  {
     let string = ''
-    let countDown = 0
+    let position = 0
     textField.value = ''
 
     // get the current value of the text
     textField.addEventListener('keyup', (event) => {
         event.preventDefault()
 
-        if (textField.value.length < 4) {
-            console.log('smaller')
+        // if (textField.value.length <= 100 && !isNaN(textField.value)) {
+        if (textField.value.length <= 5 && (!isNaN(textField.value) || textField.value.includes(':'))) {
             string += textField.value.charAt(textField.value.length - 1)
             textField.value = string
-            console.log(string)
 
-            if (string.length < 2) {
-                minutes.innerText = ''
-                minutes.innerText += string.substring(0,2)
-            }
-            else if (string.length === 2) {
-                string += ':'
-            }
-            if (string.length > 2) {
-                seconds.innerText = string.substring(3,5)
+            // add the first two numbers to the minutes field
+            if (string.length <= 2) {
+                console.log("smaller than 3")
+
+                // check if the minutes are default
+                if (minutes.innerText === '00') {
+                    minutes.innerText = ''
+                }
+
+                // add the last number character to the minutes
+                minutes.innerText += string.substring(position)
+                console.log("minutes", minutes.innerText)
+                position += 1
+
+                console.log('input: ', textField.value)
+                console.log('string: ', string)
+                console.log('string length: ', string.length)
+
+                // add the colon after the minutes have 2 numbers
+                if (minutes.innerText.length === 2) {
+                    string += ':'
+                    position += 1
+
+                }
             }
 
-        } else {
-            console.log('more')
-            console.log('string')
+            // add the last two numbers to the seconds field
+            if (string.length >= 4) {
+                
+                // remove the default values from the seconds
+                if (seconds.innerText === '00') {
+                    seconds.innerText = ''
+                }
+
+                // add the latest number values to the seconds field
+                seconds.innerText += string.substring(position)
+                position += 1
+
+                console.log('input: ', textField.value)
+                console.log('string: ', string)
+                console.log('string length: ', string.length)
+            }
+
+        } 
+        else if (string.length > 4) {
+            console.log('over now')
+            console.log('input: ', textField.value)
+            console.log('string: ', string)
+            console.log('string length: ', string.length)
             textField.readOnly = true
-            // textField.disabled = true
         }
     })
 
